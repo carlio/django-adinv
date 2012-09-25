f29130dda1ab1320f0093219af45f4dc79961e6f
carlio <git@carlcrowder.com>
2012-09-25 09:24:36 +0200

    Adding test settings

---
+++ adinv/tests/settings.py
@@ -0,0 +1,15 @@
+
+INSTALLED_APPS = ['django_jenkins', 'adinv']
+PROJECT_APPS = ['adinv']
+
+DATABASES = {
+        'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:',
+                'USER': '',
+                'PASSWORD': '',
+                'HOST': '',
+                'PORT': '',
+        }
+}
+
