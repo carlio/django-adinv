5c90a7d562f806bccf6a2ce98ff89b056736dd05
Carl Crowder <github@carlcrowder.com>
2012-09-26 18:31:10 +0200

    Mildly improving import of choosers

--- adinv/chooser/__init__.py
+++ adinv/chooser/__init__.py
@@ -1,1 +1,3 @@
-from choosers import *
+# This is done to ensure all of the choosers are registered,
+# which is a bit of a hack and probably requires a better way
+from adinv.chooser.choosers import *
