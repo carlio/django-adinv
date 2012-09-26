df1e54da72200db3f516bf5b4769f6edddc74375
carlio <git@carlcrowder.com>
2012-09-26 09:02:41 +0200

    Converting the adslot template tag to the parse/render node version to allow more complicated arguments

--- adinv/templatetags/adinv.py
+++
@@ -1,8 +0,0 @@
-from django.template import Library
-
-register = Library()
-
-def adslot():
-    pass
-
-register.inclusion_tag('adinv/inclusion/adslot.html')(adslot)
---
+++ adinv/templatetags/adslot.py
@@ -0,0 +1,36 @@
+from django import template
+from django.template import Library, Node, TemplateSyntaxError
+
+register = Library()
+
+class AdSlotNode(Node):
+    def __init__(self, slot_name, *arguments):
+        self.slot_name = slot_name
+        self.arguments = arguments
+
+    def _get_slot(self, context):
+        try:
+            slot_name = template.Variable(self.slot_name)
+            slot_name = slot_name.resolve(context)
+        except template.VariableDoesNotExist:
+            return 'noes!'
+        
+        return slot_name
+        
+    def render(self, context):
+        slot = self._get_slot(context)
+        return u'ADVERT! %s' % slot
+
+
+@register.tag("adslot")
+def adslot(parser, token):
+    parts = token.split_contents()[1:]
+    
+    if len(parts) == 0:
+        raise TemplateSyntaxError("'adslot' requires a slot name as the first argument")
+    
+    slot_name = parts[0]
+    arguments = parts[1:]
+    print type(slot_name), slot_name
+    print type(parts[1]), parts[1]
+    return AdSlotNode(slot_name, *arguments)
