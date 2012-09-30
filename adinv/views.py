33aca07cbcd508513ae1a5366815d115f6a9658f
carlio <git@carlcrowder.com>
2012-09-30 08:05:01 +0200

    Now rendering adverts inside iframe

--- adinv/views.py
+++ adinv/views.py
@@ -1,7 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+from adinv.models import Advert, AdSlot, Impression
 
 
-def advert_detail(request, advert_id):
-    pass
+def advert_detail(request, slot_id, advert_id):
+    slot = get_object_or_404(AdSlot, pk=slot_id)
+    advert = get_object_or_404(Advert, pk=advert_id)
+    
+    impression = Impression.objects.create(slot=slot, advert=advert)
+    
+    return render(request, 'adinv/advert_detail.html', {'advert': advert, 'slot': slot,
+                                                        'impression': impression})
 
 def advert_click(request, impression_id):
     pass
