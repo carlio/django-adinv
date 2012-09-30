bcef4f28505262e40e3638f52959272ab4df86de
carlio <git@carlcrowder.com>
2012-09-30 08:22:11 +0200

    Adding lots of forgotten migrations

---
+++ adinv/migrations/0007_auto__add_field_click_timestamp.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Click.timestamp'
+        db.add_column('adinv_click', 'timestamp',
+                      self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, default=datetime.datetime(2012, 9, 30, 0, 0), blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Click.timestamp'
+        db.delete_column('adinv_click', 'timestamp')
+
+
+    models = {
+        'adinv.adslot': {
+            'Meta': {'object_name': 'AdSlot'},
+            'ad_chooser': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'dimensions': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['adinv.SlotDimensions']", 'null': 'True'}),
+            'enabled': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slot_name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        },
+        'adinv.advert': {
+            'Meta': {'object_name': 'Advert'},
+            'code': ('django.db.models.fields.TextField', [], {}),
+            'dimensions': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['adinv.SlotDimensions']"}),
+            'enabled': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'adinv.click': {
+            'Meta': {'object_name': 'Click'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'impression': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['adinv.Impression']"}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
+        },
+        'adinv.impression': {
+            'Meta': {'object_name': 'Impression'},
+            'adslot': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['adinv.AdSlot']"}),
+            'advert': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['adinv.Advert']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
+        },
+        'adinv.slotdimensions': {
+            'Meta': {'object_name': 'SlotDimensions'},
+            'height': ('django.db.models.fields.SmallIntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'width': ('django.db.models.fields.SmallIntegerField', [], {})
+        }
+    }
+
+    complete_apps = ['adinv']
---
+++ adinv/migrations/0009_auto__add_field_advert_destination_url__del_field_adslot_destination_u.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Advert.destination_url'
+        db.add_column('adinv_advert', 'destination_url',
+                      self.gf('django.db.models.fields.CharField')(default='', max_length=500),
+                      keep_default=False)
+
+        # Deleting field 'AdSlot.destination_url'
+        db.delete_column('adinv_adslot', 'destination_url')
+
+
+    def backwards(self, orm):
+        # Deleting field 'Advert.destination_url'
+        db.delete_column('adinv_advert', 'destination_url')
+
+
+        # User chose to not deal with backwards NULL issues for 'AdSlot.destination_url'
+        raise RuntimeError("Cannot reverse this migration. 'AdSlot.destination_url' and its values cannot be restored.")
+
+    models = {
+        'adinv.adslot': {
+            'Meta': {'object_name': 'AdSlot'},
+            'ad_chooser': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'dimensions': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['adinv.SlotDimensions']", 'null': 'True'}),
+            'enabled': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slot_name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        },
+        'adinv.advert': {
+            'Meta': {'object_name': 'Advert'},
+            'code': ('django.db.models.fields.TextField', [], {}),
+            'destination_url': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
+            'dimensions': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['adinv.SlotDimensions']"}),
+            'enabled': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'adinv.click': {
+            'Meta': {'object_name': 'Click'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'impression': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['adinv.Impression']"}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
+        },
+        'adinv.impression': {
+            'Meta': {'object_name': 'Impression'},
+            'adslot': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['adinv.AdSlot']"}),
+            'advert': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['adinv.Advert']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
+        },
+        'adinv.slotdimensions': {
+            'Meta': {'object_name': 'SlotDimensions'},
+            'height': ('django.db.models.fields.SmallIntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'width': ('django.db.models.fields.SmallIntegerField', [], {})
+        }
+    }
+
+    complete_apps = ['adinv']
