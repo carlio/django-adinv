806bd9b418ba8a2a2e9d2ab80893fbd8c8bd9fce
carlio <git@carlcrowder.com>
2012-10-02 09:03:22 +0200

    Removing impression tracking

--- adinv/urls.py
+++ adinv/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('adinv.views',
-    url(r'^(?P<slot_id>\d+)/(?P<advert_id>\d+)$', 'advert_detail', name='advert_detail'),
-    url(r'^(?P<impression_id>\d+)/click$', 'advert_click', name='advert_click'),
+    url(r'^(?P<advert_id>\d+)$', 'advert_detail', name='advert_detail'),
 )
--- adinv/views.py
+++ adinv/views.py
@@ -1,19 +1,7 @@
-from django.shortcuts import get_object_or_404, redirect, render
-from adinv.models import Advert, AdSlot, Impression, Click
+from django.shortcuts import get_object_or_404, render
+from adinv.models import Advert
 
 
-def advert_detail(request, slot_id, advert_id):
-    slot = get_object_or_404(AdSlot, pk=slot_id)
+def advert_detail(request, advert_id):
     advert = get_object_or_404(Advert, pk=advert_id)
-    
-    impression = Impression.objects.create(adslot=slot, advert=advert)
-    
-    return render(request, advert.template_name, {'advert': advert, 'slot': slot,
-                                                        'impression': impression})
-
-def advert_click(request, impression_id):
-    impression = get_object_or_404(Impression, pk=impression_id)
-    Click.objects.create(impression=impression)
-    advert = Advert.objects.get(pk=impression.advert.id)
-    
-    return redirect(advert.destination_url)
+    return render(request, advert.template_name, {'advert': advert})
